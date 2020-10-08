b47b528e88662767ad1d9edad5508576e44c785c
admin <daominhhiep97@gmail.com>
2020-10-08 21:23:41 +0700

    controller

---
+++ src/main/java/com/casestudy/blog/controller/BlogController.java
@@ -0,0 +1,57 @@
+package com.casestudy.blog.controller;
+
+
+import com.casestudy.blog.model.Post;
+import com.casestudy.blog.model.User;
+import com.casestudy.blog.service.PostService;
+import com.casestudy.blog.service.UserService;
+import com.casestudy.blog.util.Pager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+public class BlogController {
+
+    private final UserService userService;
+
+    private final PostService postService;
+
+    @Autowired
+    public BlogController(UserService userService, PostService postService) {
+        this.userService = userService;
+        this.postService = postService;
+    }
+
+    @RequestMapping(value = "/blog/{username}", method = RequestMethod.GET)
+    public String blogForUsername(@PathVariable String username,
+                                  @RequestParam(defaultValue = "0") Optional<String> s
+            , @PageableDefault(value = 3) Pageable pageable,
+                                  Model model) {
+
+        Optional<User> optionalUser = userService.findByUsername(username);
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            Page<Post> posts = postService.findByUserOrderedByDatePageable(user, pageable);
+            Pager pager = new Pager(posts);
+
+            model.addAttribute("pager", pager);
+            model.addAttribute("user", user);
+
+            return "posts";
+
+        } else {
+            return "error";
+        }
+    }
+}
---
+++ src/main/java/com/casestudy/blog/controller/HomeController.java
@@ -0,0 +1,39 @@
+package com.casestudy.blog.controller;
+
+import com.casestudy.blog.model.Post;
+import com.casestudy.blog.service.PostService;
+import com.casestudy.blog.util.Pager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+public class HomeController {
+
+    private final PostService postService;
+
+    @Autowired
+    public HomeController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/home")
+    public String home(@RequestParam(defaultValue = "0") Optional<String> s,
+                       @PageableDefault(value = 3) Pageable pageable,
+                       Model model) {
+
+        Page<Post> posts = postService.findAllOrderedByDatePageable(pageable);
+        Pager pager = new Pager(posts);
+
+        model.addAttribute("pager", pager);
+
+        return "home";
+    }
+}
