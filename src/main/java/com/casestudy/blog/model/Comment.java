2a9a1cec3c8f32f1b098ed06fba0ad84c253d8f5
admin <daominhhiep97@gmail.com>
2020-10-08 21:22:08 +0700

    model

---
+++ src/main/java/com/casestudy/blog/model/Comment.java
@@ -0,0 +1,41 @@
+package com.casestudy.blog.model;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Column(name = "body", columnDefinition = "TEXT")
+    @NotEmpty(message = "*Please write something")
+    private String body;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_date", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Date createDate;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", referencedColumnName = "post_id", nullable = false)
+    @NotNull
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
+    @NotNull
+    private User user;
+
+}
